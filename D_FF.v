daf29f2afdb2130709659423b700eab96d9de964
SANKALPA HOTA <62276478+Sankalpa-Hota@users.noreply.github.com>
2026-02-07 20:32:15 -0800

    Update D_FF.v

--- D_FF.v
+++ D_FF.v
@@ -3,13 +3,14 @@ module DFF ( input clk , rst_n , d ,
         output qb,
         output reg q );
   assign qb = ~q;
-        always@ (posedge clk or negedge rst_n)begin   //Use or instead of , for setting conditions in @always
+  always@ (posedge clk or negedge rst_n)begin   //Use or instead of , for setting conditions in @always
     if ( !rst_n ) begin
       q  <= 0;
     end
     else begin
       q <= d;
     end
+  end
 endmodule
 
 // Industry standard is using negedge rst as asynchronous so that they reduce power loss. 1->0 has low power loss compared to 0->1
